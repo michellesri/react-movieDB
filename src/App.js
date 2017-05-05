d62969ff851a7b83b1dbda3079da40c31f0a572c
Michelle Sri <michelle.sri@gmail.com>
2017-05-05 12:14:57 -0700

    finished basic react app that calls movieDB and loads star wars movies. use npm start to run the app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import './App.css';
 
 class App extends Component {
   render() {
+    console.log('Running render! Properties:', this.props);
+    if (!this.props.loading) {
+      console.log('The movies are in:', this.props.movieList);
+      this.movieArray = this.props.movieList.Search.map(movie => (
+        <li key={movie.imdbID} >
+          {movie.Title}
+        </li>
+      ));
+    }
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Go-To-The-Movies</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {this.movieArray}
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App loading={true} />,
   document.getElementById('root')
 );
+
+fetch('http://www.omdbapi.com/?s=Star%20Wars&plot=short&r=json')
+  .then(res => res.json())
+  .then(movies => {
+    console.log('Fetched movies:', movies);
+    ReactDOM.render(
+      <App loading={false} movieList={movies}/>,
+      document.getElementById('root')
+    );
+  })
+  ;
