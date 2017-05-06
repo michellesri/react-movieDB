2dd3452c9814e3335c46b2c2b67c32578f834850
Michelle Sri <michelle.sri@gmail.com>
2017-05-06 12:19:15 -0700

    added search so that user can search for a movie in the omdb.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+let loading = true;
+
 ReactDOM.render(
-  <App loading={true} />,
+  <App loading={loading}/>,
   document.getElementById('root')
 );
 
-fetch('http://www.omdbapi.com/?s=Star%20Wars&plot=short&r=json')
+let form = document.getElementById('formButton');
+form.addEventListener('click', e => {
+  e.preventDefault();
+  readForm();
+});
+
+function App(props) {
+  if(props.loading) {
+    return (
+      <div>
+        <MovieForm />
+      </div>
+    );
+  } else {
+    const moviesList = props.movies.Search.map(movie => (
+        <Movies
+          movie={movie}
+          key={movie.imdbID}
+        />
+    )
+  );
+
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>
+              IMDBID
+            </th>
+            <th>
+              Title
+            </th>
+            <th>
+              Release Date
+            </th>
+            <th>
+              Poster
+            </th>
+          </tr>
+          {moviesList}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function Movies(props) {
+  console.log(props.movie);
+  return (
+    <tr>
+      <td>{props.movie.imdbID}</td>
+      <td>{props.movie.Title}</td>
+      <td>{props.movie.Year}</td>
+      <td><img alt="movie poster" src={props.movie.Poster} /></td>
+    </tr>
+  );
+}
+
+function MovieForm(props) {
+  return (
+    <form id="movieForm">
+      Title:<br />
+      <input type="text" name="Title" /><br />
+      <button id="formButton" type="submit" value="Submit">Submit</button>
+    </form>
+  );
+}
+
+function readForm() {
+  var formTitle = document.getElementById('movieForm').elements.item(0).value;
+  let title = '';
+  formTitle = formTitle.split(' ');
+  formTitle.forEach((str, i) => {
+    if (i < formTitle.length - 1) {
+      str += '%20';
+      title += str;
+    } else {
+      title += str;
+    }
+  });
+  let search = 'http://www.omdbapi.com/?s=' + title + '&plot=short&r=json';
+  return fetch(search)
   .then(res => res.json())
   .then(movies => {
-    console.log('Fetched movies:', movies);
+    loading = false;
     ReactDOM.render(
-      <App loading={false} movieList={movies}/>,
+      <App movies={movies} loading={loading}/>,
       document.getElementById('root')
     );
-  })
-  ;
+  });
+}
